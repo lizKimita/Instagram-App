f25a895fc288f748da2db9864d6e2250a88ab4fa
lizKimita <kimita.wanjiku@gmail.com>
2019-05-15 16:27:29 +0300

    adding urlconfigurations and initial templates

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
 
 # Create your views here.
+def welcome(request):
+    return HttpResponse('Welcome to the Moringa Tribune')
