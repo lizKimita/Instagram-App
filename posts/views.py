fbb77efa7cc0e05b3323e5b8aa9665e15168dd0f
lizKimita <kimita.wanjiku@gmail.com>
2019-05-15 16:35:11 +0300

    adding a navbar and bootstrap

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 
+
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
